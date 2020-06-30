a98476bdc5849a7b5228aea97e9e0094350aaa61
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-06-30 17:08:52 -0500

    this en varios contextos

--- multimedia_player/assets/index.js
+++ multimedia_player/assets/index.js
@@ -1,9 +1,15 @@
-import MediaPlayer from "./MediaPlayer.js"
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector("video")
 const button = document.querySelector("button")
+const buttonUnMute = document.querySelector("#buttonUnMute")
 
-const player = new MediaPlayer({el: video});
+const player = new MediaPlayer({el: video, plugins: [
+     //new AutoPlay()
+]});
 
 button.onclick = () => player.togglePlay();
 //button.onclick = () =>video.play();
+
+buttonUnMute.onclick = () => player.unmute();
