0e0a07e09743a557547c2e6a43a51936ab71e53b
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-07-05 19:00:36 -0500

    tipos de datos y funciones typescript

--- multimedia_player/assets/MediaPlayer.js
+++ multimedia_player/assets/MediaPlayer.js
@@ -1,38 +1,100 @@
-function MediaPlayer(config){
-    this.media=config.el
-    this.plugins=config.plugins || [];
-
+/*function MediaPlayer(config) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+  
     this._initPlugins();
-}
-
-MediaPlayer.prototype._initPlugins = function(){
-    this.plugins.forEach(plugin=>{
-        plugin.run(this);
+  }
+  
+  MediaPlayer.prototype._initPlugins = function() {
+    const player = {
+      play: () => this.play(),
+      pause: () => this.pause(),
+      media: this.media,
+      get muted() {
+        return this.media.muted;
+      },
+      set muted(value) {
+        this.media.muted = value;
+      },
+    };
+  
+    this.plugins.forEach(plugin => {
+      plugin.run(player);
     });
-}
-
-MediaPlayer.prototype.play = function(){
+  };
+  
+  MediaPlayer.prototype.play = function() {
     this.media.play();
-}
-
-MediaPlayer.prototype.pause = function(){
+  };
+  
+  MediaPlayer.prototype.pause = function() {
     this.media.pause();
-}
+  };
+  
+  MediaPlayer.prototype.togglePlay = function() {
+    if (this.media.paused) {
+      this.play();
+    } else {
+      this.pause();
+    }
+  };
+  
+  MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+  };
+  
+  MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
+  };
+  
+  export default MediaPlayer;
+*/
+  //convertir lo anterior a una clase
 
-MediaPlayer.prototype.togglePlay = function(){
-    if(this.media.paused)
+  class MediaPlayer {
+    constructor(config) {
+      this.media = config.el;
+      this.plugins = config.plugins || [];
+  
+      this._initPlugins();
+    }
+    _initPlugins() {
+      const player = {
+        play: () => this.play(),
+        pause: () => this.pause(),
+        media: this.media,
+        get muted() {
+          return this.media.muted;
+        },
+        set muted(value) {
+          this.media.muted = value;
+        },
+      };
+  
+      this.plugins.forEach(plugin => {
+        plugin.run(player);
+      });
+    }
+    play() {
+      this.media.play();
+    }
+    pause() {
+      this.media.pause();
+    }
+    togglePlay() {
+      if (this.media.paused) {
         this.play();
-    else
+      }
+      else {
         this.pause();
-}
+      }
+    }
+    mute() {
+      this.media.muted = true;
+    }
+    unmute() {
+      this.media.muted = false;
+    }
+  }
 
-MediaPlayer.prototype.mute = function(){
-    this.media.muted=true;
-}
-
-MediaPlayer.prototype.unmute = function(){
-    this.media.muted=false;
-}
-
-//exportar 
-export default MediaPlayer;
+  export default MediaPlayer;
--- multimedia_player/assets/index.js
+++ multimedia_player/assets/index.js
@@ -1,15 +1,28 @@
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause  from './plugins/AutoPause.ts'
 
 const video = document.querySelector("video")
 const button = document.querySelector("button")
 const buttonUnMute = document.querySelector("#buttonUnMute")
 
 const player = new MediaPlayer({el: video, plugins: [
-     //new AutoPlay()
+     new AutoPlay(), new AutoPause()
 ]});
 
 button.onclick = () => player.togglePlay();
 //button.onclick = () =>video.play();
 
-buttonUnMute.onclick = () => player.unmute();
+buttonUnMute.onclick = () => {
+     if (player.media.muted) {
+       player.unmute();
+     } else {
+       player.mute();
+     }
+   };
+   
+   if ('serviceWorker' in navigator) {
+     navigator.serviceWorker.register('/sw.js').catch(error => {
+       console.log(error.message);
+     });
+   }
